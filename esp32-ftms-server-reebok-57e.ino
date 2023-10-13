fa67079cbb74ca2092cd8406ec409841e27b5d64
dbsqp <5895806+dbsqp@users.noreply.github.com>
2023-10-14 00:37:15 +0200

    Update and rename Hall_Sensor_FTMS_server.ino to esp32-ftms-server-reebok-57e.ino
    Update filename and header info

--- esp32-ftms-server-reebok-57e.ino
+++ esp32-ftms-server-reebok-57e.ino
@@ -1,8 +1,9 @@
-
 /*
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
     Ported to Arduino ESP32 by Evandro Copercini
     updates by chegewara
+    based on esp32-ftms-server by jamesjmtaylor
+    edits for Reebok 5.7e indore exercise bike by dbsqp
 */
 
 #include <BLEDevice.h>
